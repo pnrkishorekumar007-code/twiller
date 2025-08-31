5ae48b4476b75856f11be788fcac98b7256ebaba
Bithead <80371796+BitHeadmr@users.noreply.github.com>
2025-08-31 09:55:46 +0530

    Update firebase.tsx

--- twiller/src/context/firebase.tsx
+++ twiller/src/context/firebase.tsx
@@ -1,14 +1,14 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+// keep your credentials 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrTpShJJmZUcxiHZ9o0m9ckctDFdL2VtM",
-  authDomain: "twiller-13163.firebaseapp.com",
-  projectId: "twiller-13163",
-  storageBucket: "twiller-13163.firebasestorage.app",
-  messagingSenderId: "1018495859922",
-  appId: "1:1018495859922:web:bfc66538ba8433b5cc810b",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 
