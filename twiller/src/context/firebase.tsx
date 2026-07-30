2d2c9cadf76ac752ef7cc29a754864b4f2faa898
pnrkishorekumar007 <pnrkishorekumar007@gmail.com>
2026-07-30 11:14:14 +0530

    Add Firebase config, root .gitignore, remove node_modules from tracking

--- twiller/src/context/firebase.tsx
+++ twiller/src/context/firebase.tsx
@@ -1,17 +1,20 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// keep your credentials 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyA-uxl5aNJXEVly3Jvu-RClGRKe3XlRXp0",
+  authDomain: "twiller-ef0f4.firebaseapp.com",
+  projectId: "twiller-ef0f4",
+  storageBucket: "twiller-ef0f4.firebasestorage.app",
+  messagingSenderId: "533211349682",
+  appId: "1:533211349682:web:12eef1c81ffdb536f03fab"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
