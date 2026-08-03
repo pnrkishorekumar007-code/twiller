4c93517f14fbacb5f6b38309db4bef8554d547d7
pnrkishorekumar007 <pnrkishorekumar007@gmail.com>
2026-08-03 13:54:52 +0530

    fix: force IPv4 for SMTP connections (ENETUNREACH)

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4,
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
